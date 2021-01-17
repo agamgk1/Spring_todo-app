7eea871d858a601a2f93f005ea6e380f42cf77d0
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 14:08:34 +0100

    Update SecurityConfiguration.java

--- src/main/java/com/spring/course/security/SecurityConfiguration.java
+++ src/main/java/com/spring/course/security/SecurityConfiguration.java
@@ -15,8 +15,7 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-// Rozszerzenie oraz adnotacja niezbezne do współpracyz Keyclockiem
-//@Enabled pozala nam na korzystanie z adnotacji sluzacych do zabezpieczania (w nawiasach)
+
 @EnableGlobalMethodSecurity(
         jsr250Enabled = true,
         securedEnabled = true,
@@ -25,17 +24,13 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @KeycloakConfiguration
 class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
-    //dodatkowy bean do keytlocka bez którego aplikacja nie wstanie
     @Bean
     KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
 
-    //metoda z dodatkowymi konfiguracjami m.in roli itp
-    // Autowired po to zeby spring uwzglednił ta metodę i wstrzyknął auth
     @Autowired
     void configureGlobal(AuthenticationManagerBuilder auth) {
-        //SimpleAuthorityMapper Służy do zmiany/edycji, mapowania rol. Role (nazwy) w springu zaczynają się od "ROLE_"
         var authorityMapper = new SimpleAuthorityMapper();
         authorityMapper.setPrefix("ROLE_");
         authorityMapper.setConvertToUpperCase(true);
@@ -44,25 +39,18 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
         auth.authenticationProvider(keycloakProvider);
     }
 
-    //Metoda służąca do obsługi rejestracji urzytkownika
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    //metoda służaca do zabezpieczenia określonych informacji - w tym wypadku będzie to /info/*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        //do /info/* może wejść tylko użytkownik z rolą USER (pojawi się ekran logowania) - musi być z dużych liter
         http.authorizeRequests()
                .antMatchers("/info/*")
                .hasRole("USER")
-            //    .antMatchers("/projects")
-           //     .hasRole("ADMIN")
                 .anyRequest()
                 .permitAll();
     }
-
 }
-
