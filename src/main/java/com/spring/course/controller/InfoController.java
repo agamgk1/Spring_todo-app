ed21fc33e38218c55b5eb8e98449076bc989b6d5
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:45:25 +0100

    Update InfoController.java

--- src/main/java/com/spring/course/controller/InfoController.java
+++ src/main/java/com/spring/course/controller/InfoController.java
@@ -12,25 +12,19 @@ import javax.annotation.security.RolesAllowed;
 @RestController
 @RequestMapping("/info")
 class InfoController {
-    // @value zasilenie pola propertisem. W klamrach nazwa propertisa
-    //DataSourceProperties specjalna klsa do uzywania propertisow. Sama pobiera propertisy z .yml
-
+    
     private DataSourceProperties dataSource;
     private TaskConfigurationProperties myProp;
-
-    //wstrzykiwanie propertisow przez konstruktor
+    
     InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
         this.dataSource = dataSource;
         this.myProp = myProp;
     }
-    // w nawiasie rola z jaką można wejsc do danej metody (na dana strone)
     @Secured("ROLE_ADMIN")
     @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
-
-    //to samo co @Security ale z pakietu java
     @RolesAllowed({"ROLE_USER, ROLE_ADMIN"})
     @GetMapping("/prop")
     boolean myProp() {
