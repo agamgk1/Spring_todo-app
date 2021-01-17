66eb792b3ec66c195e8ea670b8604cfa42e07766
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:44:53 +0100

    Update IllegalExceptionProcessing.java

--- src/main/java/com/spring/course/controller/IllegalExceptionProcessing.java
+++ src/main/java/com/spring/course/controller/IllegalExceptionProcessing.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//@retention - jak długo ta adnotacja ma pozostawac
-//@Target - umozliwaia konfiguracje adnotacji - co mozemy adnotowac tą adnotacją
-//.RUNTIME - jest zachowana rwniez wtedy jak aplikacja jest uruchomiona
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @interface IllegalExceptionProcessing {
