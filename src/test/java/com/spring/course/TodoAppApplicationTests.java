10a0e1ac71c291d13878c5a7810e7e377e2c7ebb
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 14:13:43 +0100

    Update TodoAppApplicationTests.java

--- src/test/java/com/spring/course/TodoAppApplicationTests.java
+++ src/test/java/com/spring/course/TodoAppApplicationTests.java
@@ -3,14 +3,10 @@ package com.spring.course;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// dzięki tej adnotacji faktycznie uruchamia sie cala aplikacjia (caly konteks springowy)
 @SpringBootTest
 class TodoAppApplicationTests {
 
-	//testo ładujący kontekst springowy - test bazowy
 	@Test
 	void contextLoads() {
 	}
-
-
 }
