a1b3412bb032883a0d1e5684b47da08fb2864310
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 14:06:41 +0100

    Update ChangedTaskEventListener.java

--- src/main/java/com/spring/course/reports/ChangedTaskEventListener.java
+++ src/main/java/com/spring/course/reports/ChangedTaskEventListener.java
@@ -8,18 +8,15 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-// serwisy związane są z procesowaniem, usługowością aplikacji
 @Service
 class ChangedTaskEventListener {
     private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
-
     private final PersistedTaskEventRepository repository;
 
     ChangedTaskEventListener(PersistedTaskEventRepository repository) {
         this.repository = repository;
     }
-
-    //Adnotacja do obslugi zdarzen + włączenie asynchronicznego procesowania zdarzenia (poza głównym wątkiem)
+    
     @Async
     @EventListener
     void on(TaskDone event) {
