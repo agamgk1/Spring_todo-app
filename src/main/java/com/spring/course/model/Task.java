757d2a64a3a1375cd58c0e51288eb6545e0168d5
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 14:04:30 +0100

    Update Task.java

--- src/main/java/com/spring/course/model/Task.java
+++ src/main/java/com/spring/course/model/Task.java
@@ -6,32 +6,23 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
-//oznacza że w tej klasie bedzie tworzona tabela bazy danych
 @Entity
 @Table(name = "tasks") // nazwa tabeli
 public class Task  {
-    //te pola zostana zmapowane na kolumny table "tasks
-    // pole id musi miec specjalne oznaczenie (hibernate musi je odroznic)
-    // adnotacja generated - umozliwia generowanie id
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    //wtedy kolumna w bazie bedzie nazywała sie desc zamias description column umozliwia rowniez inne ustawienia
-  //  @Column(name = "desc") - hibernate
-    @NotBlank(message = "Task's description must not be null and not empty")  //adnotacja służąca do validacji - zaklada ze opis (pole) nie moze byc pusty albo null albo same spacj itp
+    @NotBlank(message = "Task's description must not be null and not empty") 
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    // osadzenie klasy Audit ktora jest embdabled. Czyli pola z klasy Audit beda dostepne tutaj
     @Embedded
     private Audit audit = new Audit();
-    // umozliwia zapis wielu do jednej grupy (wiele taskow do jednej grupy) + wskazanie grupy do join
     @ManyToOne
     @JoinColumn(name = "task_group_id")
     private TaskGroup group;
 
-
-    //konstruktor potrzebny jest do hibernate
    public Task() {
     }
 
@@ -39,7 +30,6 @@ public class Task  {
         this(description, deadline, null);
     }
 
-    // konstruktor ktory pozwoli stworzyc taska na podstawie description i deadline i taskGroup
     public Task(@NotBlank(message = "Task's description must not be null and not empty") String description, LocalDateTime deadline, TaskGroup group) {
         this.description = description;
         this.deadline = deadline;
