3d1d77ee1944c814ef850b243b937dc1b76a60ca
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:42:34 +0100

    Update SqlTaskGroupRepository.java

--- src/main/java/com/spring/course/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/spring/course/adapter/SqlTaskGroupRepository.java
@@ -8,15 +8,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//interfejs (springowy) słuzący do komunikacji z bazą danych (repozytorium) dla klasy (tabeli) TaskGroup
-//rozszerza JpaRepository ze springa oraz przypisanie encji Id (id jest integerem) z klasy Task. Dodatkowo rozszeza nasz interfejs z publicznym repozytorium
-//JpaRepository - repozytorium sql. Każda metoda będzie tłumaczona na zapytanie sqlowe
-
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
-    // w query znajduje sie zapytanie na encjach (w tym przypadku nazwa encji to nazwa klasy)
-    // tasks to pole z klasy TaskGroup
-    // fetch zabezpieczenie przez lazy loadingiem
+  
     @Override
     @Query("from TaskGroup g join fetch g.tasks ")
     List<TaskGroup> findAll();
