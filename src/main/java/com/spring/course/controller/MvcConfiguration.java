36ad85856ce94a2cfd5d7a691ff3118daf2d0fb1
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:46:27 +0100

    Update MvcConfiguration.java

--- src/main/java/com/spring/course/controller/MvcConfiguration.java
+++ src/main/java/com/spring/course/controller/MvcConfiguration.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.Set;
 
-//klasa bedaca konfiguracja springowa
-// + implematecja WebMvcConfiguration, ktory umozliwi dodanie interceptora (LoggerInterceptor)
 @Configuration
 class MvcConfiguration implements WebMvcConfigurer {
     private Set<HandlerInterceptor> interceptors;
@@ -16,8 +13,6 @@ class MvcConfiguration implements WebMvcConfigurer {
     MvcConfiguration(Set<HandlerInterceptor> interceptors) {
         this.interceptors = interceptors;
     }
-
-    //dodanie interceptora
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //dodanie wszystkich interceptorow do kontekstu springowego
