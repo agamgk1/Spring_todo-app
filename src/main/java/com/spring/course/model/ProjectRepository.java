0fa0911de2d4f3ee6c4888bcbb5bedd016e26675
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:53:54 +0100

    Update ProjectRepository.java

--- src/main/java/com/spring/course/model/ProjectRepository.java
+++ src/main/java/com/spring/course/model/ProjectRepository.java
@@ -3,7 +3,6 @@ package com.spring.course.model;
 import java.util.List;
 import java.util.Optional;
 
-// publiczne repozytorium z metodami udostepnionymi publicznie dla klasy (tabeli) TaskGroup
 public interface ProjectRepository {
 
     List<Project> findAll();
