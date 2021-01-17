de01138bcd192d82832fd6813b3c21712a7207dd
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:44:34 +0100

    Update IllegalExceptionControllerAdvice.java

--- src/main/java/com/spring/course/controller/IllegalExceptionControllerAdvice.java
+++ src/main/java/com/spring/course/controller/IllegalExceptionControllerAdvice.java
@@ -4,8 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// adnotacja ktora pozwala na obsługę błędów z innych kontrolerów, które będzą oznaczone adnotacja IllegalExceptionProcessing. Każdy kontroler w ktorym pojawi sie illegalStateException
-// będzie obsłużony tutaj
 @RestControllerAdvice(annotations = IllegalExceptionProcessing.class)
 class IllegalExceptionControllerAdvice {
 
