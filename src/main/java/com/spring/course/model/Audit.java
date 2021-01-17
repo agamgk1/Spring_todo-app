150b1962f0c1f16219ee74daecd7ca4e6c356f18
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:53:12 +0100

    Update Audit.java

--- src/main/java/com/spring/course/model/Audit.java
+++ src/main/java/com/spring/course/model/Audit.java
@@ -5,20 +5,15 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-// adnotacja @MappedSuperclass oznacza ze jest to klasa bazowa ktora pozwoli na modelowanie na bazie danych
-// @ Embedable oznaczaze ta klasa jest do osadzenia w innym miejscu
 @Embeddable
 class Audit {
-    // kolumny pomocnicze - nie beda widoczne w JSON
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
 
-    //adnotacja spowoduje uruchominie metody przed zapisem do bazy danych
     @PrePersist
     void prePersist() {
         createdOn = LocalDateTime.now();
     }
-    // odpowiedzialna za aktualizacje encji
     @PreUpdate
     void preMarge() {
         updatedOn = LocalDateTime.now();
