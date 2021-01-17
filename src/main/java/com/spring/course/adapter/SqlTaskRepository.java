588b8140da541649c879da37d1f0c6082bca297b
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:43:04 +0100

    Update SqlTaskRepository.java

--- src/main/java/com/spring/course/adapter/SqlTaskRepository.java
+++ src/main/java/com/spring/course/adapter/SqlTaskRepository.java
@@ -9,18 +9,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//interfejs (springowy) słuzący do komunikacji z bazą danych (repozytorium) dla klasy (tabeli) Task
-//rozszerza JpaRepository ze springa oraz przypisanie encji Id (id jest integerem) z klasy Task. Dodatkowo rozszeza nasz interfejs z publicznym repozytorium
-//JpaRepository - repozytorium sql. Każda metoda będzie tłumaczona na zapytanie sqlowe
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
-    //nadpisanie metody oraz wykorzystanie w niej sql. musza byc obie adnotacje
-    // metody existsby sa to metody ze springa - spring podpowiada
     @Override
     @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=?1")
     boolean existsById(@Param("id") Integer id);
-    // wyszukanie niezrobionych taskow w obrebie grupy
+  
     @Override
     boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
 
