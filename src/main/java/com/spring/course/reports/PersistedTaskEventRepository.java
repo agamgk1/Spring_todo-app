d793ca234bf33097c6c4ca71f522755ceb0c75db
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 14:07:22 +0100

    Update PersistedTaskEventRepository.java

--- src/main/java/com/spring/course/reports/PersistedTaskEventRepository.java
+++ src/main/java/com/spring/course/reports/PersistedTaskEventRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-//integer bo id jest typu int
 interface PersistedTaskEventRepository extends JpaRepository<PersistedTaskEvent, Integer> {
     List<PersistedTaskEvent> findByTaskId(int taskId);
 }
