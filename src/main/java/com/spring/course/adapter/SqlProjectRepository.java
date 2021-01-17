7a9c5f0bd58f6a501dd531bb991b04764a1045e0
Adam Gierek <64829285+agamgk1@users.noreply.github.com>
2021-01-17 13:42:03 +0100

    Update SqlProjectRepository.java

--- src/main/java/com/spring/course/adapter/SqlProjectRepository.java
+++ src/main/java/com/spring/course/adapter/SqlProjectRepository.java
@@ -8,15 +8,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//interfejs (springowy) słuzący do komunikacji z bazą danych (repozytorium) dla klasy (tabeli) TaskGroup
-//rozszerza JpaRepository ze springa oraz przypisanie encji Id (id jest integerem) z klasy Task. Dodatkowo rozszeza nasz interfejs z publicznym repozytorium
-//JpaRepository - repozytorium sql. Każda metoda będzie tłumaczona na zapytanie sqlowe
-
 @Repository
 interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
-    // w query znajduje sie zapytanie na encjach (w tym przypadku nazwa encji to nazwa klasy)
-    // tasks to pole z klasy TaskGroup
-    // fetch zabezpieczenie przez lazy loadingiem
     @Override
     @Query("from Project p join fetch p.steps")
     List<Project> findAll();
